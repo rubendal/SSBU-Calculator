2c2567ca69255602a21eb734a1e8fb755ea1e38e
Ruben <rubendalssb@gmail.com>
2018-12-02 01:14:52 -0600

    Fix rage multiplier not being displayed

--- js/calculator.js
+++ js/calculator.js
@@ -773,7 +773,7 @@ app.controller('calculator', function ($scope) {
 			resultList.push(new Result("KB dealt", "x" + +attacker.modifier.kb_dealt.toFixed(6)));
 		}
 		if (!ignoreStale)
-			resultList.push(new Result("Rage","x" + +Rage(attacker_percent).toFixed(6), true));
+			resultList.push(new Result("Rage","x" + +Rage(attacker_percent).toFixed(6)));
 		resultList.push(new Result("Total KB", +vskb.kb.toFixed(6)));
 		if ($scope.kb_modifier == "buried") {
 			resultList.push(new Result("Buried removed", vskb.kb >= parameters.buried_kb_threshold ? "Yes" : "No"));
@@ -855,15 +855,18 @@ app.controller('calculator', function ($scope) {
                 //resultList.push(new Result("Full HP shield", +(50 * target.modifier.shield).toFixed(6), +(50 * target.modifier.shield).toFixed(6)));
 				resultList.push(new Result("Shield Break", sv >= 50 * target.modifier.shield ? "Yes" : "No"));
 			}
+
+			var is_aerial_move = $scope.selected_move != null ? $scope.selected_move.aerial : false;
+
 			resultList.push(new Result("Shield Hitlag", ShieldHitlag(StaleDamage(damageOnShield, stale, ignoreStale), hitlag, electric)));
-			resultList.push(new Result("Shield stun", ShieldStun(StaleDamage(damageOnShield, stale, ignoreStale), is_projectile, perfectshield)));            
-			resultList.push(new Result("Shield Advantage", ShieldAdvantage(StaleDamage(damageOnShield, stale, ignoreStale), hitlag, hitframe, $scope.use_landing_lag == "yes" ? faf + landing_lag : $scope.use_landing_lag == "autocancel" ? faf + attacker.attributes.hard_landing_lag : faf, is_projectile, electric, perfectshield)));
+			resultList.push(new Result("Shield stun", ShieldStun(StaleDamage(damageOnShield, stale, ignoreStale), is_projectile, perfectshield, is_aerial_move)));
+			resultList.push(new Result("Shield Advantage", ShieldAdvantage(StaleDamage(damageOnShield, stale, ignoreStale), hitlag, hitframe, $scope.use_landing_lag == "yes" ? faf + landing_lag : $scope.use_landing_lag == "autocancel" ? faf + attacker.attributes.hard_landing_lag : faf, is_projectile, electric, perfectshield, is_aerial_move)));
 
 			if (!windbox) {
 				if (!is_projectile)
 					resultList.push(new Result("Attacker shield pushback", +AttackerShieldPushback(StaleDamage(damageOnShield, stale, ignoreStale)).toFixed(6)));
 
-				resultList.push(new Result("Target shield pushback", +(ShieldPushback(StaleDamage(damageOnShield, stale, ignoreStale), is_projectile, perfectshield)).toFixed(6), s >= 50 * target.modifier.shield, sv >= 50 * target.modifier.shield));
+				resultList.push(new Result("Target shield pushback", +(ShieldPushback(StaleDamage(damageOnShield, stale, ignoreStale), is_projectile, perfectshield, is_aerial_move)).toFixed(6), sv >= 50 * target.modifier.shield));
 			}
         } else {
             resultList.push(new Result("Unblockable attack", "Yes"));
