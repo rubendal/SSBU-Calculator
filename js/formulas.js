5f45579f792c816e81b1e38922440e500486c185
Ruben <rubendalssb@gmail.com>
2019-10-31 21:18:30 -0600

    Missed changes on parry hitlag formula

--- js/formulas.js
+++ js/formulas.js
@@ -369,12 +369,12 @@ function ParryHitlag(base_damage, hitlag_mult, electric, is_projectile, players)
 		var p = [1, 0.925, 0.862, 0.8116, 0.77464, 0.752464, 0.75];
 		player_mult = p[players - 2];
 	}
-	if (hitlag_mult > 0.67) {
-		hitlag_mult *= 0.88;
-	}
+	//if (hitlag_mult > 0.67) {
+	//	hitlag_mult *= 0.88;
+	//}
 	var h = 0;
 	if (!is_projectile)
-		h = Math.floor((((base_damage * parameters.hitlag.mult * player_mult + parameters.hitlag.constant + parameters.hitlag.parryConstant) * electric_mult) * hitlag_mult));// - 1;
+		h = Math.floor((((base_damage * parameters.hitlag.mult * player_mult + parameters.hitlag.parryConstant) * electric_mult) * hitlag_mult));// - 1;
 	else
 		h = Math.floor((((base_damage * parameters.hitlag.mult * player_mult + parameters.hitlag.constant) * electric_mult) * hitlag_mult));// - 1;
 	if (h > parameters.hitlag.parryMax + 2) {
